e32b1c19eb63a6b54d5f70b0a8dd4860b1678e26
Sebastian Just <Sebastian.Just@sungard.com>
2014-04-27 19:18:37 +0200

    first commit

---
+++ smslib/src/main/java/org/smslib/gateway/proxy/ProxyGateway.java
@@ -0,0 +1,12 @@
+package org.smslib.gateway.proxy;
+
+import org.smslib.gateway.AbstractGateway;
+import org.smslib.message.DeliveryReportMessage;
+import org.smslib.message.DeliveryReportMessage.DeliveryStatus;
+import org.smslib.message.InboundMessage;
+import org.smslib.message.OutboundMessage;
+import org.smslib.message.OutboundMessage.SentStatus;
+
+public class ProxyGateway extends AbstractGateway {
+
+}
