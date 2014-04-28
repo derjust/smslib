ea52adaf54db8919fd322c3552e9e507040f8925
Sebastian Just <Sebastian.Just@sungard.com>
2014-04-28 09:45:36 +0200

    Redirect jersey's jul logging to slf4j

--- smslib/src/main/java/org/smslib/gateway/proxy/ProxyGateway.java
+++ smslib/src/main/java/org/smslib/gateway/proxy/ProxyGateway.java
@@ -9,6 +9,7 @@ import org.glassfish.jersey.moxy.json.MoxyJsonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger; 
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.smslib.core.Coverage;
 import org.smslib.core.CreditBalance;
 import org.smslib.gateway.AbstractGateway;
@@ -47,9 +48,18 @@ public class ProxyGateway extends AbstractGateway {
 			throw new IllegalArgumentException(e.getMessage(), e);
 		} 
 		
+		//Redirect Jersey's logging - levels must still be specified via jul's logging.properties!
+		// Optionally remove existing handlers attached to j.u.l root logger
+		 SLF4JBridgeHandler.removeHandlersForRootLogger();  // (since SLF4J 1.6.5)
+
+		 // add SLF4JBridgeHandler to j.u.l's root logger, should be done once during
+		 // the initialization phase of your application
+		 SLF4JBridgeHandler.install();
+		
 		//Register endpoints
 		config = new ResourceConfig(MessagesEndpoint.class, VersionEndpoint.class);
-		//Register binding to this proxy gateway from the endpoints
+
+		//Register binding to this proxy gateway for the endpoints
 		config.register(new AbstractBinder()
 		{
             @Override
@@ -58,20 +68,22 @@ public class ProxyGateway extends AbstractGateway {
                 bindFactory(new ProxyGatewayFactory(ProxyGateway.this)).to(ProxyGateway.class);
             }
         });
+		
+		//Register Moxy feature for Json
 		config.register(MoxyJsonFeature.class);
 	    config.register(JsonMoxyConfigurationContextResolver.class);
 	     
+	    //Enable security token only if present
 	    if (token != null && token.length() > 0)
 	    {
 	    	logger.info("Authorization token '{}' activated", token);
 	    	config.register(new SecurityFilter(token));
 	    }
-		
 	}
 
 	public ProxyGateway(String gatewayId, String... parms)
 	{
-		this(gatewayId, parms[0], Integer.parseInt(parms[1]), parms.length >= 2 ? parms[2]: null);
+		this(gatewayId, parms[0], Integer.parseInt(parms[1]), parms.length >= 2 ? parms[2] : null);
 	}
 	
 	@Override
@@ -102,14 +114,14 @@ public class ProxyGateway extends AbstractGateway {
 	}
 
 	@Override
-	protected CreditBalance _queryCreditBalance() throws Exception {
+	protected CreditBalance _queryCreditBalance() {
 		CreditBalance cb = new CreditBalance();
 		cb.setCredits(Double.NEGATIVE_INFINITY);
 		return cb;
 	}
 
 	@Override
-	protected Coverage _queryCoverage(Coverage coverage) throws Exception {
+	protected Coverage _queryCoverage(Coverage coverage) {
 		coverage.setCoverage(false);
 		return coverage;
 	}
